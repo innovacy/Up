edb0919c6ccf24299af234b1f0e2508a4cdccfe3
Dimitrios Karvounaris <d.karvounaris@innovacy.com>
2015-10-07 18:48:43 +0200

    Refactoring run() code in semantic methods

--- Up.php
+++ Up.php
@@ -17,77 +17,59 @@ use Innovacy\Up\Navigation;
  */
 class Up
 {
-    /**
-     * @var MarkDown
-     */
+    protected $virtualUri;
+    /** @var MarkDown The parser class */
     protected $parserNavigation;
 
-    /**
-     * @var MarkDown
-     */
+    /** @var MarkDown The parser class */
     protected $parserMain;
 
-    /**
-     * @var MarkDown
-     */
+    /** @var MarkDown The parser class */
     protected $parserFooter;
+
+    /** @var string Base local path */
+    protected $basePath;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->basePath = dirname(__FILE__);
+        $this->virtualUri = $this->getVirtualUri();
+
+        $this->parserMain = new Markdown();
+        $this->parserMain->html5 = true;
+
+        $this->parserNavigation = new Navigation();
+        $this->parserNavigation->html5 = true;
+
+        $this->parserFooter = new Markdown();
+        $this->parserFooter->html5 = true;
+    }
     
     /**
      * This is the main app
      */
     public function run()
     {
-        $uri = $this->getVirtualUri();
-        $filePath = dirname(__FILE__);
 
         $checkNeedsUpdate = false;
         $redirectToHtml = false;
-        if (is_file($filePath.$uri.'.md')) {
-            $file = $filePath . $uri . '.md';
-        } elseif (is_file($filePath.str_replace('.html', '', $uri).'.md')) {
-            $file = $filePath.str_replace('.html', '', $uri).'.md';
-            // todo needs to check if html exists and updateable
-# Recognize all supported linking styles from possible markdown documents outside, using mdwiki or not converted by Up!
-        } elseif (is_file($filePath.$uri.'.html')) {
-            // this should never be reached now
-            $file = $filePath . $uri;
-            $checkNeedsUpdate = true;
-            $redirectToHtml = true;
-        } elseif (is_file($filePath.str_replace('.md', '', $uri).'.html')) {
-            $file = $filePath . str_replace('.md', '', $uri);
-            $checkNeedsUpdate = true;
-            $redirectToHtml = true;
-        } elseif (is_file($filePath.$uri)) {
-            $file = $filePath.$uri;
-            // TODO: remove! This (#!) isn't working, it's not possible to parse the fragment at all, so we only can change url's we encounter in documents, but not serve any that is called this way
-            // TODO #2: Once a page is created (as example index.html) a javascript might be able to recognize if (index.html#!something.md) were called and sent this to the server recognizable
-        } elseif (is_file($filePath.str_replace('#!', '', $uri))) {
-            $file = $filePath.str_replace('#!', '', $uri);
-        } elseif (is_file($filePath.$uri.'/index.md')) {
-            $file = $filePath.$uri.'/index.md';
-        } elseif (is_file($filePath.'/404.md')) {
-            $file = $filePath.'/404.md';
-        } else {
-            echo '404 File not found'; // TODO: Improve!
-        }
+        $file = $this->lookupFile();
 
-        $this->parserMain = new Markdown();
-        $this->parserMain->html5 = true;
         $markdown = file_get_contents($file);
         $markup = $this->parserMain->parse($markdown);
 
-        $this->parserNavigation = new Navigation();
-        $navigation = $this->parserNavigation->parse(file_get_contents($filePath.'/navigation.md'));
+        $navigation = $this->parserNavigation->parse(file_get_contents($this->basePath .'/navigation.md'));
         $css = '';
-        if (is_file($filePath.'/css/innovacy.css')) {
+        if (is_file($this->basePath .'/css/innovacy.css')) {
             $css = '<link rel="stylesheet" href="'.'/css/innovacy.css'.'" type="text/css">';
         }
 
         $footer = '';
-        if (is_file($filePath.'/footer.md')) {
-            $this->parserFooter = new Markdown();
-            $this->parserFooter->html5 = true;
-            $footer = $this->parserFooter->parse(file_get_contents($filePath . '/footer.md'));
+        if (is_file($this->basePath .'/footer.md')) {
+            $footer = $this->parserFooter->parse(file_get_contents($this->basePath . '/footer.md'));
         }
 
         return <<<HTML
@@ -114,7 +96,11 @@ $footer
 HTML;
     }
 
-    public function getVirtualUri()
+    /**
+     * Returns the cleaned up uri
+     * @return string
+     */
+    private function getVirtualUri()
     {
         $scriptName = $_SERVER['SCRIPT_NAME'];
         $requestUri = $_SERVER['REQUEST_URI'];
@@ -132,6 +118,51 @@ HTML;
         $pathInfo = parse_url($pathInfo, PHP_URL_PATH);
         $pathInfo = '/' . ltrim($pathInfo, '/');
         return $pathInfo;
+    }
+
+    /**
+     * Checks all valid forms of a virtualUri and tries to find a matching file
+     * @return string
+     */
+    public function lookupFile()
+    {
+        if (is_file($this->basePath . $this->virtualUri . '.md')) {
+            $file = $this->basePath . $this->virtualUri . '.md';
+            return $file;
+        } elseif (is_file($this->basePath . str_replace('.html', '', $this->virtualUri) . '.md')) {
+            $file = $this->basePath . str_replace('.html', '', $this->virtualUri) . '.md';
+            return $file;
+            // todo needs to check if html exists and updateable
+# Recognize all supported linking styles from possible markdown documents outside, using mdwiki or not converted by Up!
+        } elseif (is_file($this->basePath . $this->virtualUri . '.html')) {
+            // this should never be reached now
+            $file = $this->basePath . $this->virtualUri;
+            $checkNeedsUpdate = true;
+            $redirectToHtml = true;
+            return $file;
+        } elseif (is_file($this->basePath . str_replace('.md', '', $this->virtualUri) . '.html')) {
+            $file = $this->basePath . str_replace('.md', '', $this->virtualUri);
+            $checkNeedsUpdate = true;
+            $redirectToHtml = true;
+            return $file;
+        } elseif (is_file($this->basePath . $this->virtualUri)) {
+            $file = $this->basePath . $this->virtualUri;
+            return $file;
+            // TODO: remove! This (#!) isn't working, it's not possible to parse the fragment at all, so we only can change url's we encounter in documents, but not serve any that is called this way
+            // TODO #2: Once a page is created (as example index.html) a javascript might be able to recognize if (index.html#!something.md) were called and sent this to the server recognizable
+        } elseif (is_file($this->basePath . str_replace('#!', '', $this->virtualUri))) {
+            $file = $this->basePath . str_replace('#!', '', $this->virtualUri);
+            return $file;
+        } elseif (is_file($this->basePath . $this->virtualUri . '/index.md')) {
+            $file = $this->basePath . $this->virtualUri . '/index.md';
+            return $file;
+        } elseif (is_file($this->basePath . '/404.md')) { // TODO: Separate "not found" logic from looking up file
+            $file = $this->basePath . '/404.md';
+            return $file;
+        } else {
+            echo '404 File not found'; // TODO: No output!
+            return $file; // TODO: Improve!
+        }
     }
 
 }
