4f1ef95e86a4e5b8c121fdbd127d8d1488510add
Dimitrios Karvounaris <d.karvounaris@innovacy.com>
2015-10-09 05:13:10 +0200

    Table bootstrap rendering

--- Up/Markdown.php
+++ Up/Markdown.php
@@ -12,5 +12,41 @@ use cebe\markdown\GithubMarkdown;
 
 class Markdown extends GithubMarkdown
 {
+    private $tableCellTag = 'td';
+    private $tableCellCount = 0;
+    private $tableCellAlign = [];
 
+    protected function renderTable($block)
+    {
+        $content = '';
+        $this->tableCellAlign = $block['cols'];
+        $content .= "<thead>\n";
+        $first = true;
+        foreach ($block['rows'] as $row) {
+            $this->tableCellTag = $first ? 'th' : 'td';
+            $align = empty($this->tableCellAlign[$this->tableCellCount]) ? '' : ' align="' . $this->tableCellAlign[$this->tableCellCount] . '"';
+            $this->tableCellCount++;
+            $tds = "<$this->tableCellTag$align>" . trim($this->renderAbsy($this->parseInline($row))) . "</$this->tableCellTag>"; // TODO move this to the consume step
+            $content .= "<tr>$tds</tr>\n";
+            if ($first) {
+                $content .= "</thead>\n<tbody>\n";
+            }
+            $first = false;
+            $this->tableCellCount = 0;
+        }
+        return "<table class=\"table table-bordered\">\n$content</tbody>\n</table>\n";
+    }
+
+    /**
+     * @marker |
+     */
+    protected function parseTd($markdown)
+    {
+        if (isset($this->context[1]) && $this->context[1] === 'table') {
+            $align = empty($this->tableCellAlign[$this->tableCellCount]) ? '' : ' align="' . $this->tableCellAlign[$this->tableCellCount] . '"';
+            $this->tableCellCount++;
+            return [['text', "</$this->tableCellTag><$this->tableCellTag$align>"], isset($markdown[1]) && $markdown[1] === ' ' ? 2 : 1]; // TODO make a absy node
+        }
+        return [['text', $markdown[0]], 1];
+    }
 }
